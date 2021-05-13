e10acf37a3be51ace56c9c47926b56d6cd8516ab
Desktop42 <36649761+Metafern@users.noreply.github.com>
2021-05-13 15:37:49 -0700

    Update mouser.py

--- mouser.py
+++ mouser.py
@@ -1,4 +1,11 @@
 import requests
+url = 'https://www.nanostring.com/blog/of-mice-and-men-the-importance-of-mouse-models-in-research/'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+result = requests.get(url, headers=headers)
 
-x = requests.get('https://w3schools.com')
-print(x.status_code)
+webpage = result.content.decode()
+
+print(webpage)
+
+if(webpage.find("<i class=\"mouse-month-mouse\"") != -1):
+	print("FOUND!")
